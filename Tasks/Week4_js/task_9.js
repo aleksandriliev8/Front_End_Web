acf681ce5973c7635733df2803072635b6df9299
Aleksandar Iliev <aleksandriliev8@gmail.com>
2024-12-02 22:03:32 +0200

    Created task_9.js

---
+++ Tasks/Week4_js/task_9.js
@@ -0,0 +1,17 @@
+function dynamicPropertyAccess(str_array)
+{
+    //Not finished
+    let template = `${key1}.${key2}=${value}`;
+    let result = null;
+
+    result.key1
+}
+
+const str_array = [
+    "user.name=Nicole",
+    "user.age=24",
+    "address.city=New York",
+    "address.zip=10001"
+]
+
+
